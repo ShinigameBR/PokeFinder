e80801a609e9156bfdbcf60e63766022ed74df0e
ShinigameBR <shinigamecontato@gmail.com>
2022-07-24 10:30:26 -0300

    Update

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -7,27 +7,54 @@ form.addEventListener("submit", function (e) {
   let searchUrl = (apiUrl + pokemon).toLowerCase();
   let responseContent = document.getElementById("pokemonContent");
   let responseImages = document.getElementById("pokemonImages");
-  let response;
 
   fetch(searchUrl)
     .then((responseContent) => responseContent.json())
     .then(function (data) {
       let name = "Nome: " + upperFirstLetter(data.name) + "<br>";
-      let types = "Types: ";
+      let types = "Tipo: ";
       for (let i = 0; i < data.types.length; i++) {
         types += upperFirstLetter(data.types[i].type.name);
         if (i == 0 && data.types.length == 2) {
           types += ", ";
         }
       }
-      response = name + types;
-      responseContent.innerHTML = response;
-      responseImages.innerHTML =
-        '<img src="' +
-        data.sprites.front_default +
-        '" alt="Imagem da Frente do Pokémon."><img src="' +
-        data.sprites.back_default +
-        '" alt="Imagem das Costas do Pokémon.">';
+      let id = "<br> Número na Pokédex: " + data.id;
+      responseContent.innerHTML = name + types + id;
+      if (
+        data["sprites"]["versions"]["generation-v"]["black-white"]["animated"][
+          "front_default"
+        ] != null &&
+        data["sprites"]["versions"]["generation-v"]["black-white"]["animated"][
+          "back_default"
+        ] != null
+      ) {
+        responseImages.innerHTML =
+          '<img src="' +
+          data["sprites"]["versions"]["generation-v"]["black-white"][
+            "animated"
+          ]["front_default"] +
+          '" alt="Imagem da Frente do Pokémon."><img src="' +
+          data["sprites"]["versions"]["generation-v"]["black-white"][
+            "animated"
+          ]["back_default"] +
+          '" alt="Imagem das Costas do Pokémon.">';
+      } else if (
+        data.sprites.front_default != null &&
+        data.sprites.back_default != null
+      ) {
+        responseImages.innerHTML =
+          '<img src="' +
+          data.sprites.front_default +
+          '" alt="Imagem da Frente do Pokémon."><img src="' +
+          data.sprites.back_default +
+          '" alt="Imagem das Costas do Pokémon.">';
+      } else {
+        responseImages.innerHTML =
+          '<img src="https://assets.pokemon.com/assets/cms2/img/pokedex/detail/' +
+          data.id +
+          '.png" alt="Imagem da Frente do Pokémon.">';
+      }
     })
     .catch(function (error) {
       responseContent.innerHTML =
